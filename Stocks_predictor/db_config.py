9566419b5e5991a38c192e89139df3497628229b
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2026-04-19 07:49:24 +0200

    read DB config from an ini file
    add the ini file to Git's ignore list

--- Stocks_predictor/db_config.py
+++ Stocks_predictor/db_config.py
@@ -1,11 +1,22 @@
+import os
+import configparser
 import psycopg2
+
+
+# Get the path to config.ini relative to this script
+config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+
+# Load configuration from config.ini
+config = configparser.ConfigParser()
+config.read(config_path)
+
 # use PGADMIN-SOFTWARE for simplicity 
 # Run the Schema.sql as it is firstly in ur database(using Query Tool) -> then use the same database for DB_CONFIGURATION
-PG_HOST = "localhost"       
-PG_PORT = 5432
-PG_DB   = "YOUR_DATABASE_NAME"
-PG_USER = "YOUR_DATABASE_USER_NAME"    
-PG_PASS = "YOUR_DATABSE_PASSWORD"   
+PG_HOST = config.get('database', 'host')
+PG_PORT = config.getint('database', 'port')
+PG_DB   = config.get('database', 'dbname')
+PG_USER = config.get('database', 'user')
+PG_PASS = config.get('database', 'password')
 
 def create_connection():
     conn = psycopg2.connect(
