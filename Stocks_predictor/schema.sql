102384bcdd7519f0e7ccfc0f5d70236001e4f6e3
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2026-04-21 19:23:51 +0200

    increase number of digits for float values
    increase allowed length of field company name
    
    Yahoo! delivers high precision numbers. The have up to 17 digits after
    the decimal point. So, we use this in our database. Setting the total
    number of digits to 30, this leaves us with 13 digits in front of the
    decimal point which should be more than enough.
    
    The PostgreSQL docs say
    
    In this syntax:
      The precision is the total number of digits
      The scale is the number of digits in the fraction part.
      The storage type of the numeric type depends on the precision and scale.
    
    The NUMERIC type can hold a value of up to 131,072 digits before the decimal point 16,383 digits after the decimal point.

--- Stocks_predictor/schema.sql
+++ Stocks_predictor/schema.sql
@@ -4,17 +4,17 @@ DROP TABLE IF EXISTS stock_prices CASCADE;
 DROP TABLE IF EXISTS companies CASCADE;
 CREATE TABLE companies (
  company_id SERIAL PRIMARY KEY,
- company_name VARCHAR(100) NOT NULL ,
+ company_name VARCHAR(200) NOT NULL ,
  ticker_symbol VARCHAR(200) UNIQUE NOT NULL 
 );
 CREATE TABLE stock_prices (
     id SERIAL PRIMARY KEY,
     company_id INTEGER NOT NULL REFERENCES companies(company_id) ON DELETE CASCADE,
     trade_date DATE NOT NULL,
-    open_price NUMERIC(12, 4),
-    high_price NUMERIC(12, 4),
-    low_price NUMERIC(12, 4),
-    close_price NUMERIC(12, 4),
+    open_price NUMERIC(30,17),
+    high_price NUMERIC(30,17),
+    low_price NUMERIC(30,17),
+    close_price NUMERIC(30,17),
     volume BIGINT,
     UNIQUE(company_id, trade_date)
 );
