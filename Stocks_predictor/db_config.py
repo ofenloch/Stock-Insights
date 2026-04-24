5e5279e95fa12c1910dd2dd20790670b4b44aef4
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2026-04-24 13:08:23 +0200

    add function set_up_database
    This function creates the user and the database for Stock-Insights if they don't exist.
    It uses the admin connection to execute the necessary SQL commands.
    The admin connection is read from the ini file.

--- Stocks_predictor/db_config.py
+++ Stocks_predictor/db_config.py
@@ -1,5 +1,6 @@
 import os
 import configparser
+from tkinter.filedialog import test
 import psycopg2
 
 
@@ -19,11 +20,11 @@ PG_USER = config.get('database', 'user')
 PG_PASS = config.get('database', 'password')
 
 
-ADMIN_HOST=config.get('admin', 'host')
-ADMIN_PORT=config.getint('admin', 'port')
-ADMIN_DB=config.get('admin', 'dbname')
-ADMIN_USER=config.get('admin', 'user')
-ADMIN_PASS=config.get('admin', 'password')
+ADMIN_HOST=config.get('dbadmin', 'host')
+ADMIN_PORT=config.getint('dbadmin', 'port')
+ADMIN_DB=config.get('dbadmin', 'dbname')
+ADMIN_USER=config.get('dbadmin', 'user')
+ADMIN_PASS=config.get('dbadmin', 'password')
 
 def create_connection() -> psycopg2.extensions.connection:
     conn = psycopg2.connect(
@@ -44,3 +45,43 @@ def create_admin_connection() -> psycopg2.extensions.connection:
         password=ADMIN_PASS
     )
     return conn
+
+def set_up_database():
+    """
+        This function creates the user and the database for Stock-Insights
+        if they don't exist.
+        
+        It uses the admin connection to execute the necessary SQL commands.
+    """
+    
+    admin_conn = create_admin_connection()
+    admin_conn.autocommit = True
+
+    with admin_conn.cursor() as admin_cursor:
+        # Mimick the CREATE USER IF NOT EXISTS
+        # see https://stackoverflow.com/a/55954480
+        psql_script_1 = f"""
+            DO $$
+            BEGIN
+                CREATE ROLE {PG_USER} WITH LOGIN PASSWORD '{PG_PASS}';
+            EXCEPTION WHEN duplicate_object THEN
+                RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+            END
+            $$;
+        """
+        admin_cursor.execute(psql_script_1)
+        admin_conn.commit()
+        print(f"User {PG_USER} created (or already existed)")
+
+        # The concept from above can't be used with CREATE DATABASE because "CREATE DATABASE cannot run inside a transaction block"
+        try:
+            admin_cursor.execute(f"CREATE DATABASE {PG_DB} OWNER {PG_USER};")
+            print(f"Database {PG_DB} created, owner is {PG_USER}")
+        except psycopg2.errors.DuplicateDatabase:
+            print(f"Database {PG_DB} already exists, skipping creation.")
+
+        admin_conn.commit() 
+
+    admin_conn.close()
+    print("Database and user created successfully!")
+    
